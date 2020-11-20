62cee7cd5fe342ab603749368210018016366da5
zsugabubus <zsugabubus@users.noreply.github.com>
2020-11-20 21:11:50 +0100

    Update default config of net block

--- config.def.h
+++ config.def.h
@@ -7,7 +7,7 @@ static Block BLOCKS[] =
 	{ 2, block_cpu, "", "CPU %p" },
 	{ 2, block_memory, NULL, "MEM %u/%t (%p)" },
 	{ 3, block_fsstat, "/home", "%n: %a (%F)" },
-	{ 4, block_net, "lo", "%n: DN %R @ %r UP %T @ %t" },
+	{ 4, block_net, "lo", "%n: RX %R @ %r TX %T @ %t" },
 	{ 5, block_datetime, NULL, "%c" }
 	{ 6, block_spawn,
 		"while true; do for c in a b c; do echo -n $c; sleep 1; done; echo; echo; sleep 1; done\0",
